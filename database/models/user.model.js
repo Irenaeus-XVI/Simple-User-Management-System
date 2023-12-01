427630f8ce6b514df75587184faf28dfca74bb94
Ehab-Tarek <ehabtarek166@gmail.com>
2023-12-01 13:53:36 +0200

    Finish User Model

---
+++ database/models/user.model.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+
+
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    }
+}, { timestamps: true })
+
+
+export const userModel = model('user', userSchema)
